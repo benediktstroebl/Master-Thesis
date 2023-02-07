e1e4f8f6adc4bb5676e52bf06217ba805711450e
Benedikt Ströbl <stroebl.benedikt@gmail.com>
2023-02-07 09:10:02 +0100

    modified CRS loading parameter in gdf creation methods to string.

--- load_data_module.py
+++ load_data_module.py
@@ -44,15 +44,15 @@ raw_trip_ep_gdf = raw_full_trip_gdf.drop(['geometry', 'TRIP_SP'], axis=1)
 raw_trip_ep_gdf['LONG'], raw_trip_ep_gdf['LAT']  = raw_trip_ep_gdf['TRIP_EP'].apply(lambda x: x.x), raw_trip_ep_gdf['TRIP_EP'].apply(lambda x: x.y)
 
 # Convert EP and SP file to GeoDataFrame
-raw_trip_ep_gdf = gp.GeoDataFrame(raw_trip_ep_gdf, geometry="TRIP_EP", crs = 4326)
-raw_trip_sp_gdf = gp.GeoDataFrame(raw_trip_sp_gdf, geometry="TRIP_SP", crs = 4326)
+raw_trip_ep_gdf = gp.GeoDataFrame(raw_trip_ep_gdf, geometry="TRIP_EP", crs="EPSG:4326")
+raw_trip_sp_gdf = gp.GeoDataFrame(raw_trip_sp_gdf, geometry="TRIP_SP", crs="EPSG:4326")
 
 # Project all GDFs to CRS 3035
-raw_trip_ep_gdf = raw_trip_ep_gdf.to_crs(3035)
-raw_trip_sp_gdf = raw_trip_sp_gdf.to_crs(3035)
-raw_full_trip_gdf = raw_full_trip_gdf.to_crs(3035)
-raw_points_gdf = raw_points_gdf.to_crs(3035)
-tesselation_gdf = tesselation_gdf.to_crs(3035)
+raw_trip_ep_gdf = raw_trip_ep_gdf.to_crs("EPSG:3035")
+raw_trip_sp_gdf = raw_trip_sp_gdf.to_crs("EPSG:3035")
+raw_full_trip_gdf = raw_full_trip_gdf.to_crs("EPSG:3035")
+raw_points_gdf = raw_points_gdf.to_crs("EPSG:3035")
+tesselation_gdf = tesselation_gdf.to_crs("EPSG:3035")
 
 # drop SP and EP columns from trip file
 raw_full_trip_gdf = raw_full_trip_gdf.drop(['TRIP_SP', 'TRIP_EP'], axis=1)
